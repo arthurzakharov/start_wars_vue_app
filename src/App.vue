fdda9296aa2bb9b0e434dacca8adc35842dd66d5
arthurzakharov <a.zakharov@inchkiev.ua>
2019-02-05 12:27:35 +0200

    rm last default components

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,6 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <div id="app"></div>
 </template>
 
 <style lang="scss">
-@import "./styles/styles.scss";
-#app {
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-family: $f_bold_ital;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
