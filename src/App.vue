549a655b3f295e5dc2e23ccc73f37ac414fd42b2
arthurzakharov <a.zakharov@inchkiev.ua>
2019-02-05 12:33:38 +0200

    basic structure

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
-  <div id="app"></div>
+  <div class="app">
+    <header class="header"></header>
+    <main class="main">
+      <aside class="aside"></aside>
+      <article class="page"></article>
+    </main>
+    <footer class="footer"></footer>
+  </div>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
+  .app {
+  
+  }
 </style>
