7d69c452cf87770ca72119fed64353847ee3fe9f
arthurzakharov <a.zakharov@inchkiev.ua>
2019-02-07 15:17:55 +0200

    add navigation menu

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,46 @@
     <header class="header">
       <AHamburger />
       <ALogo />
+      <nav v-if="isMobile" class="mobile-menu">
+        <ul class="mobile-menu__list">
+          <li class="mobile-menu__item">
+            <router-link class="mobile-menu__link" exact to="/">Home</router-link>
+          </li>
+          <li class="mobile-menu__item">
+            <router-link class="mobile-menu__link"exact to="/films">Films</router-link>
+          </li>
+          <li class="mobile-menu__item">
+            <router-link class="mobile-menu__link"exact to="/people">People</router-link>
+          </li>
+          <li class="mobile-menu__item">
+            <router-link class="mobile-menu__link"exact to="/planets">Planets</router-link>
+          </li>
+          <li class="mobile-menu__item">
+            <router-link class="mobile-menu__link"exact to="/species">Species</router-link>
+          </li>
+          <li class="mobile-menu__item">
+            <router-link class="mobile-menu__link"exact to="/starships">Starships</router-link>
+          </li>
+          <li class="mobile-menu__item">
+            <router-link class="mobile-menu__link"exact to="/vehicles">Vehicles</router-link>
+          </li>
+        </ul>
+      </nav>
     </header>
     <main class="main">
       <aside v-if="isPC" class="aside"></aside>
-      <article class="page"></article>
+      <article class="page">
+        <div class="page__item"></div>
+        <div class="page__item"></div>
+        <div class="page__item"></div>
+        <div class="page__item"></div>
+        <div class="page__item"></div>
+        <div class="page__item"></div>
+        <div class="page__item"></div>
+        <div class="page__item"></div>
+        <div class="page__item"></div>
+        <div class="page__item"></div>
+      </article>
     </main>
     <footer class="footer">
       <p class="footer__text">
@@ -46,15 +82,59 @@
     justify-content: space-between;
   }
   .header {
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    right: 0;
     padding: 20px 10px;
     background-color: $c_black-deep;
     display: flex;
     align-items: center;
     justify-content: space-between
   }
+  .mobile-menu {
+    position: fixed;
+    z-index: 1;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    background-color: $c_black-deep;
+    &__list {
+      margin-top: 75px;
+      margin-left: 10px;
+      background-color: transparent;
+    }
+    &__item {
+      margin-bottom: 30px;
+      background-color: transparent;
+      &:last-of-type { margin-bottom: 0 }
+    }
+    &__link {
+      background-color: transparent;
+      font-size: 52px;
+      font-weight: 700;
+      color: $c_yellow;
+      text-decoration: none;
+      text-transform: uppercase;
+      &.active {
+        color: white;
+      }
+    }
+  }
   .main {
-    flex-grow: 1;
+    margin-top: 75px;
     background-color: $c_black;
+  }
+  .page {
+    background-color: $c_black;
+    &__item {
+      width: 90%;
+      height: 60px;
+      margin: 20px auto;
+      background-color: dimgray;
+    }
   }
   .footer {
     display: flex;
--- src/router.js
+++ src/router.js
@@ -7,6 +7,8 @@ Vue.use(Router);
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
+  linkActiveClass: 'active',
+  linkExactActiveClass: 'exact',
   routes: [
     {
       path: '/',
